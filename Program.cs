2c4018bfc962a76b1fd6bf234ad8201b31a6fd25
huntsman95 <huntsman95@gmail.com>
2023-01-12 17:46:03 -0700

    Implemented method for uploading/downloading binary data

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 using Microsoft.ApplicationInsights.DataContracts;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.FileProviders;
+using Namotion.Reflection;
 using System.Collections.ObjectModel;
 using System.Management.Automation;
 using System.Management.Automation.Runspaces;
 using System.Reflection.PortableExecutable;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -26,10 +28,12 @@ var path = Path.Combine(
 //Our whole app is basically middleware
 app.Run(async (context) => {
 
+    //Allow us to read the POST data in raw format
     context.Request.EnableBuffering();
 
+    //Define session state for PWSH
     InitialSessionState sessionState = InitialSessionState.CreateDefault();
-    sessionState.ExecutionPolicy = Microsoft.PowerShell.ExecutionPolicy.Unrestricted;
+    sessionState.ExecutionPolicy = Microsoft.PowerShell.ExecutionPolicy.Unrestricted; //This will fail if you try and containerize the app. Remove this line if so.
 
     //Expose Query String to PWSH like PHP
     sessionState.Variables.Add(new SessionStateVariableEntry("_GET", context.Request.Query, "HTTP Query String Dictionary", ScopedItemOptions.Constant));
@@ -41,20 +45,19 @@ app.Run(async (context) => {
     var HeaderDict = new Dictionary<string, string>();
     sessionState.Variables.Add(new SessionStateVariableEntry("_HEADERS", HeaderDict, "HTTP Header Dictionary", ScopedItemOptions.AllScope));
 
-
     //Expose Path to Controller
     sessionState.Variables.Add(new SessionStateVariableEntry("_REQUEST_PATH", context.Request.Path, "HTTP Path String", ScopedItemOptions.Constant));
     sessionState.Variables.Add(new SessionStateVariableEntry("_REQUEST_METHOD", context.Request.Method, "HTTP Method String", ScopedItemOptions.Constant));
 
-
+    //Easily access form data in PWSH using $_POST['key']
     if (context.Request.ContentType != null && context.Request.ContentType.StartsWith("application/x-www-form-urlencoded"))
     {
         sessionState.Variables.Add(new SessionStateVariableEntry("_POST", context.Request.Form, "HTTP Post String Dictionary", ScopedItemOptions.Constant));
     }
 
-    if (context.Request.ContentType != null && (context.Request.ContentType.StartsWith("application/json") || context.Request.ContentType.StartsWith("application/x-www-form-urlencoded")))
+    //Write raw JSON data to PWSH to allow us to host REST API's etc
+    if (context.Request.ContentType != null && context.Request.ContentType.StartsWith("application/json"))
     {
-        // Leave the body open so the next middleware can read it.
         using (var reader = new StreamReader(
             context.Request.Body,
             encoding: Encoding.UTF8,
@@ -63,20 +66,29 @@ app.Run(async (context) => {
             leaveOpen: true))
         {
             var body = await reader.ReadToEndAsync();
-            // Do some processing with body…
-
-            // Reset the request body stream position so the next middleware can read it
             context.Request.Body.Position = 0;
-            sessionState.Variables.Add(new SessionStateVariableEntry("_RAWPOSTDATA", body, "HTTP Post Data", ScopedItemOptions.Constant));
+            sessionState.Variables.Add(new SessionStateVariableEntry("_JSONDATA", body, "HTTP JSON Post Data", ScopedItemOptions.Constant));
         }
     }
 
-    //Set default status code
+    //Set Microsoft.AspNetCore.WebUtilities.FileBufferingReadStream $_RAWPOSTDATASTREAM in PWSH to allow us to parse file uploads etc with our own code
+    if (null != context.Request.Body)
+    {
+        sessionState.Variables.Add(new SessionStateVariableEntry("_RAWPOSTDATASTREAM", context.Request.Body, "HTTP Post Data Stream", ScopedItemOptions.None)); //try to pipe stream directly to PWSH
+    }
+
+    //Set default status code of 200 OK
     sessionState.Variables.Add(new SessionStateVariableEntry("_STATUSCODE", 200, "Status Code for HTTP Response", ScopedItemOptions.AllScope));
+
+    //Instantiate Binary Data Byte Array
+    sessionState.Variables.Add(new SessionStateVariableEntry("_BINARYRESPONSE", new byte[0], "Status Code for HTTP Response", ScopedItemOptions.AllScope));
 
 
     string OutputBuffer = "";
     string ContentType = "text/html";
+    byte[] PWSHBinaryResponse = new byte[0];
+
+    List<byte> tmpBytes = new();
 
     using (PowerShell PowerShellInst = PowerShell.Create(sessionState))
     {
@@ -88,25 +100,30 @@ app.Run(async (context) => {
             StringBuilder stringBuilder = new();
             foreach (PSObject obj in results)
             {
-                if (obj != null)
+                if(null != obj && obj.BaseObject is byte) //allow easy output of binary data - note: this is SLOW; use 
                 {
-                    stringBuilder.AppendLine(obj.ToString());
+                    tmpBytes.Add((byte)obj.BaseObject);
+                }
+                else if (null != obj)
+                {
+                    stringBuilder.AppendLine(obj.ToString());                    
                 }
             }
 
-            if (PowerShellInst.HadErrors && (Environment.GetEnvironmentVariable("PWSHCGI_DEBUG") == "TRUE"))
+            StringBuilder errorBuilder = new();
+            if (PowerShellInst.HadErrors && (Environment.GetEnvironmentVariable("DEBUG") == "TRUE"))
             {
                 foreach (var error in PowerShellInst.Streams.Error)
                 {
-                    stringBuilder.AppendLine("<div id=\"pwshErrorDiv\" style=\"background:#FFF !important; color:#000 !important;\"><pre>");
-                    stringBuilder.AppendLine(error.ScriptStackTrace.ToString());
-                    stringBuilder.AppendLine(error.ToString());
-                    stringBuilder.AppendLine("</pre></div>");
+                    errorBuilder.AppendLine(error.ToString());
+                    errorBuilder.AppendLine(error.ScriptStackTrace.ToString());
                 }
             }
 
-            OutputBuffer = stringBuilder.ToString();
+            PWSHBinaryResponse = (byte[])PowerShellInst.Runspace.SessionStateProxy.GetVariable("_BINARYRESPONSE");
+
             Dictionary<string, string> PWSHHeaders = (Dictionary<string,string>)PowerShellInst.Runspace.SessionStateProxy.GetVariable("_HEADERS");
+
             if (PWSHHeaders != null)
             {
                 foreach(var header in PWSHHeaders)
@@ -119,18 +136,49 @@ app.Run(async (context) => {
                 }
             }
 
+            //If content is HTML output formatted errors else just output errors
+            if (errorBuilder.Length > 0)
+            {
+                if (context.Response.Headers.ContainsKey("Content-Type") && (context.Response.Headers["Content-Type"].ToString().ToUpper() == "TEXT/HTML"))
+                {
+                    stringBuilder.AppendLine("<div id=\"pwshErrorDiv\" style=\"background:#FFF !important; color:#000 !important;\"><pre>");
+                    stringBuilder.AppendLine(errorBuilder.ToString());
+                    stringBuilder.AppendLine("</pre></div>");
+                }
+                else
+                {
+                    stringBuilder.AppendLine(errorBuilder.ToString());
+                }
+            }
+
             //Allow us to configure a status code in PWSH
             int PWSHStatusCode = (int)PowerShellInst.Runspace.SessionStateProxy.GetVariable("_STATUSCODE");
             context.Response.StatusCode = PWSHStatusCode;
 
+            //Write script output to buffer
+            OutputBuffer = stringBuilder.ToString();
         }
         catch (Exception ex)
         {
+            // If we encounter an ASP.Net Core error vs a PWSH error, write it to the web-browser.
+            context.Response.StatusCode = 500; //Tell the web browser we encountered an error
             OutputBuffer = ex.Message;
         }
     }
     // Send PWSH Output to Browser
-    await context.Response.WriteAsync(OutputBuffer);
+
+    if (PWSHBinaryResponse.Length > 0) //Fast binary output with PS Variable
+    {
+        await context.Response.Body.WriteAsync(PWSHBinaryResponse,0,PWSHBinaryResponse.Length);
+    }
+    else if (tmpBytes.Count > 0) //Slow binary output with PS Obj List but easier to script if you don't read the documentation
+    {
+        await context.Response.Body.WriteAsync(tmpBytes.ToArray(), 0, tmpBytes.Count);
+    }
+    else //If we are writing string data (default)
+    {
+        await context.Response.WriteAsync(OutputBuffer);
+    }
 
 });
 
--- README.md
+++ README.md
@@ -4,20 +4,18 @@
 This ASP.NET Core "Shim" provides a fast, responsive way to serve a PowerShell based MVC website. If you are looking for a dead-simple way to execute and serve individual `.ps1` scripts in IIS and do not care about speed, please check out my `PWSHCGI7` project on GitHub
 
 ## Installation
-Ensure the .Net Core hosting package is installed in IIS
-
-Publish this project to a folder
-
-Point IIS to that folder when making a new site
-
-Change the Application Pool to Unmanaged as this is .Net Core so .Net4.0 CLR etc will just slow everything down
+- Ensure the .Net Core hosting package is installed in IIS
+- Publish this project to a folder
+- Point IIS to that folder when making a new site
+- Change the Application Pool to Unmanaged
 
 ## Variables exposed to PowerShell
 ### Read-Only Variables
 - `$_GET` - use like PHP `$variable = $_GET['somevariable']`
 - `$_POST` - use like PHP `$variable = $_POST['somevariable']`
 - `$_COOKIE` - use like PHP `$variable = $_COOKIE['somevariable']`
-- `$_RAWPOSTDATA` - used to access the RAW body of the POST/PUT/PATCH request
+- `$_JSONDATA` - Raw POST data unparsed if request `content-type` = `application/json`
+- `$_RAWPOSTDATASTREAM` - used to access the RAW stream of `context.Request.Body` (File Uploads / etc)
 - `$_REQUEST_METHOD` - Will equal something like `GET` or `POST`
 - `$_REQUEST_PATH` - Will contain the request path (e.g. `/` , `/home` , `/api/something`)
 
@@ -75,3 +73,56 @@ return $response #return response to ASP.NET Core shim for delivery back to IIS
 
 ## Static Content (.js / .css / etc)
 Put static content that should be available without authentication in the `public` directory under `PwshWeb`
+
+## Dynamically Serving Files / Binary Data
+### Summary
+Provides a method to serve binary data using authentication / other logic.
+### Method 1 (preferred)
+Set PWSH variable `$_BINARYRESPONSE` to a Byte-Array value (`[byte[]]`)
+
+Example:
+```powershell
+# controller.ps1
+
+$_HEADERS.Add("Content-type", "image/png")
+$_BINARYRESPONSE = [System.IO.File]::ReadAllBytes("C:\somepath\image.png")
+```
+
+### Method 2 (slow)
+Return a byte array directly to the pipeline
+
+Due to how the PWSH SDK works, the output of arrays is returned one at a time to the pipeline. This results in a very large `PSDataCollection<PSObject>` collection containing individual `Byte` objects that need to be added to a `List<byte>` in a `foreach` loop and then returned by ASP.Net Core.
+
+It is for this reason that this method should only be used for small byte arrays.
+
+Example:
+```powershell
+# controller.ps1
+
+$_HEADERS.Add("Content-type", "image/png")
+return [byte[]][System.IO.File]::ReadAllBytes("C:\somepath\image.png")
+```
+
+## Debugging Powershell Scripts
+Set Environment Variable `DEBUG` to `TRUE` for PWSH Error Output on your web pages.
+
+Example in IIS `web.config`:
+```xml
+<?xml version="1.0" encoding="utf-8"?>
+<configuration>
+  <location path="." inheritInChildApplications="false">
+    <system.webServer>
+      <handlers>
+        <add name="aspNetCore" path="*" verb="*" modules="AspNetCoreModuleV2" resourceType="Unspecified" />
+      </handlers>
+      <aspNetCore processPath="dotnet" arguments=".\AspnetCorePWSH.dll" stdoutLogEnabled="false" stdoutLogFile=".\logs\stdout" hostingModel="inprocess">
+      <!--INSERT BELOW THIS LINE-->
+        <environmentVariables>
+          <environmentVariable name="DEBUG" value="TRUE" />
+        </environmentVariables>
+      <!--INSERT ABOVE THIS LINE-->
+      </aspNetCore>
+    </system.webServer>
+  </location>
+</configuration>
+```
